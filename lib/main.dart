22fa87b302216ede11a5d2f16aa8efea308ce200
arghaahit <argha.ahit@gmail.com>
2023-10-17 18:43:33 +0530

    changs

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:admin/constants.dart';
 import 'package:admin/controllers/MenuAppController.dart';
 import 'package:admin/screens/main/main_screen.dart';
-import 'package:admin/screens/orders/main.orders.dart';
 import 'package:admin/screens/team/main.team.dart';
 import 'package:admin/screens/Clients/main.client.dart';
-import 'package:admin/screens/user/main.use.dart';
 import 'package:admin/screens/venture/main.venture.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -33,7 +31,7 @@ class MyApp extends StatelessWidget {
             create: (context) => MenuAppController(),
           ),
         ],
-        child: MainScreen(),
+        child: TeamMain(),
       ),
     );
   }
--- lib/screens/Clients/main.client.dart
+++ lib/screens/Clients/main.client.dart
@@ -11,26 +11,28 @@ import 'package:provider/provider.dart';
 class MainUSer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: context.read<MenuAppController>().scaffoldKey,
-      drawer: SideMenu(title: 'Team', context: context,),
-      body: SafeArea(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // We want this side menu only for large screen
-            if (Responsive.isDesktop(context))
+    return Material(
+      child: Scaffold(
+        key: context.read<MenuAppController>().scaffoldKey,
+        drawer: SideMenu(title: 'Team', context: context,),
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // We want this side menu only for large screen
+              if (Responsive.isDesktop(context))
+                Expanded(
+                  // default flex = 1
+                  // and it takes 1/6 part of the screen
+                  child: SideMenu(title: 'Team', context: context,),
+                ),
               Expanded(
-                // default flex = 1
-                // and it takes 1/6 part of the screen
-                child: SideMenu(title: 'Team', context: context,),
+                // It takes 5/6 part of the screen
+                flex: 5,
+                child: ClientsList(),
               ),
-            Expanded(
-              // It takes 5/6 part of the screen
-              flex: 5,
-              child: ClientsList(),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
