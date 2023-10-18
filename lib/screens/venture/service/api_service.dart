d89a003d7804ac797c332772f0973a3915180099
arghaahit <argha.ahit@gmail.com>
2023-10-18 14:44:32 +0530

    changes

---
+++ lib/screens/venture/service/api_service.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:admin/screens/venture/model/model.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: 'https://5d42a6e2bc64f90014a56ca0.mockapi.io/api/v1/')
+abstract class RestClient {
+  factory RestClient(Dio dio) = _RestClient;
+
+  @GET('/tasks')
+  Future<List<Task>> getTasks();
+}
+
